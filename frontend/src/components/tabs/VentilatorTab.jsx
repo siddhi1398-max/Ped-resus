41316c26f591c6793bb810266d5af4c2262ffb33
siddhi1398-max <siddhi1398@gmail.com>
2026-05-01 13:54:59 +0530

    Update VentilatorTab.jsx

--- frontend/src/components/tabs/VentilatorTab.jsx
+++ frontend/src/components/tabs/VentilatorTab.jsx
@@ -8,7 +8,6 @@
 
 import { useState, useMemo } from "react";
 import { useWeight } from "../../context/WeightContext";
-import { ICD_INSERTION, complications } from "../../data/criticalCare";
 import {
   Warning, Lightbulb, ArrowRight, CaretDown, CheckCircle,
   XCircle, Info, Wrench, Wind, Drop,
